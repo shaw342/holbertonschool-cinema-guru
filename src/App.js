7e5beb6274873c4faf0030e7194ca549361d94d8
shaw342 <shawanbarua70@gmail.com>
2024-10-31 12:00:26 +0100

    task 3:add post request with axios en useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Input from './routes/general/Input';
 import SelectInput from './routes/general/SelectInput';
 import Button from './routes/general/button';
 import SearchBar from './routes/general/SearchBar';
+import axios from 'axios';
 
 
 function App() {
   const option = ["Latest","Oldest","Highest","Lowest Rated"]
   const [value,setInputValue] = useState("")
   const [selectValue,setSelectValue] = useState("")
-  const [valueSearch,setValueSearch] = useState("")
+  const [title,setTitle] = useState("")
+  const [isLoggedIn,setIsLoggedIn] = useState(false)
+  const [username,setUsername] = useState("")
+  useEffect(()=>{
+    const accessToken  = localStorage.getItem("accessToken")
+    if (accessToken){
+      const authenticate = async ()=>{
+        try {
+          const reponse = await axios.post("/api/auth",{},{
+            headers:{
+              "Authorization":`Bearer ${accessToken}`
+            }
+          })
+          setIsLoggedIn(true)
+          setUsername(reponse.data.username)
+        } catch (error) {
 
+          console.log(error);
+          
+          
+        }
+      }
+      authenticate()
+    }
 
+  },[])
   return (
     <div className="App">
       <Input label="username" type="username" className="email" value={value} setvalue={setInputValue} />
       <SelectInput className="Sort" label="Sort" option={option} value={selectValue} setValue={setSelectValue} />
-      <Button className="Button-load" label="Load More.." onClick={onClick()}/>
-      <SearchBar title="Search Movies" value={valueSearch} setvalue={setValueSearch}/>
+      <Button className="Button-load" label="Load More.." />
+      <SearchBar value={title}  setvalue={setTitle}/>
     </div>
   );
 }
